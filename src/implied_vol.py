aa9f5938044021bb9d186d885c70d89af161b766
Basavaraja G <basava@inchara.local>
2026-05-17 13:23:19 +0530

    refactor: improve IV surface with configurable filters and griddata interpolation

--- src/implied_vol.py
+++ src/implied_vol.py
@@ -6,10 +6,22 @@ from scipy.optimize import brentq
 from src.black_scholes import bs_option_price
 
 
-def fetch_option_chain(ticker: str, expiry: str) -> pd.DataFrame:
+def fetch_option_chain(
+    ticker: str,
+    expiry: str,
+    min_bid: float = 0.0,
+    min_volume: int = 0,
+    min_open_interest: int = 0,
+    max_spread_pct: float = 0.8,
+    min_moneyness: float = 0.7,
+    max_moneyness: float = 1.3,
+    max_spread_abs: float = 1.0,
+    use_open_interest: bool = True,
+    verbose: bool = False,
+) -> pd.DataFrame:
     """
     Fetches the option chain data using yfinance api, for the given ticker and
-    expiry.
+    expiry. It applys various filters with user defined values.
 
     Parameters
     ----------
@@ -17,11 +29,29 @@ def fetch_option_chain(ticker: str, expiry: str) -> pd.DataFrame:
         The ticker symbol of the stock or the underlying.
     expiry : str
         Expiry date of the option. Must be in the ISO format 'YYYY-MM-DD`. eg. '2022-02-23'
+    min_bid : float, optional
+        Minimum bid price. Options with zero bid have no active buyer, by default 0.0
+    min_volume : int, optional
+        Minimum volume of the option trade, by default 0
+    min_open_interest : int, optional
+        Minimum open interest of the option, by default 0
+    max_spread_pct : float, optional
+        Maximim bid-ask spread, by default 0.8
+    min_moneyness : float, optional
+        Minimum moneyness: min(strike/spot), by default 0.7
+    max_moneyness : float, optional
+        Maximum moneyness max(strike/spot), by default 1.3
+    max_spread_abs : float, optional
+        Absolute maximum bid-ask spread, not percentage, by default 1.0.
+    use_open_interest : bool, optional
+        Whether to use Open interest filter rather volume filter, by default True
+    verbose : bool, optional
+        Print information of each filter, by default False
 
     Returns
     -------
-    option_chain : pd.DataFrame
-        The dataframe of the option chain containing columns spot, strike, expiry,
+    pd.DataFrame
+        The dataframe of the option chain containing columns spot, strike, moneyness, expiry,
         mid_price, option_type, and time_to_expiry
     """
     tk = yfinance.Ticker(ticker)
@@ -32,18 +62,66 @@ def fetch_option_chain(ticker: str, expiry: str) -> pd.DataFrame:
     puts["option_type"] = "put"
     df = pd.concat([calls, puts], ignore_index=True)
 
+    if verbose:
+        print("Number of options")
+        print("-----------------")
+        print(f"Raw optioins: {len(df)}")
+
     df["mid_price"] = (df["bid"] + df["ask"]) / 2.0
 
+    spot = tk.fast_info["last_price"]
+    df["spot"] = spot
+
+    df["moneyness"] = df["strike"] / spot
+
     # liquidity filters: keep only liquid options
-    df = df[df["bid"] > 0]
+    df = df[df["bid"] > min_bid]
     df = df[df["ask"] > 0]
-    df = df[df["volume"] > 0]
-    df = df[(df["ask"] - df["bid"]) / df["mid_price"] < 0.5]  # spread < 50% of mid
+    if verbose:
+        print(f"After bid filter: {len(df)}")
+
+    if use_open_interest:
+        df = df[df["openInterest"] >= min_open_interest]
+    else:
+        df = df[df["volume"] > min_volume]
+
+    if verbose:
+        print(f"After volume or OI filter: {len(df)}")
+
+    df = df[
+        (df["ask"] - df["bid"]) / df["mid_price"] < max_spread_pct
+    ]  # spread < 50% of mid
+    if verbose:
+        print(f"After spread filter: {len(df)}")
+
+    df = df[(df["moneyness"] >= min_moneyness) & (df["moneyness"] <= max_moneyness)]
+    if verbose:
+        print(f"After moneyness filter: {len(df)}")
+
+    df["spread"] = df["ask"] - df["bid"]
+    # df = df[df["spread"] < max_spread_abs]  # maximum $1 wide bid-ask
+    df = df[
+        df["spread"] < np.maximum(max_spread_abs, df["mid_price"] * max_spread_pct)
+    ]  # maximum $1 wide bid-ask
+
+    if verbose:
+        print(f"After absolute spread filter: {len(df)}")
 
     df["time_to_expiry"] = (pd.to_datetime(expiry) - pd.Timestamp.today()).days / 365.25
     df["expiry"] = expiry
     df = df.dropna()
-    return df[["strike", "expiry", "mid_price", "option_type", "time_to_expiry"]]
+
+    return df[
+        [
+            "spot",
+            "strike",
+            "moneyness",
+            "expiry",
+            "mid_price",
+            "option_type",
+            "time_to_expiry",
+        ]
+    ]
 
 
 def compute_implied_vol(
